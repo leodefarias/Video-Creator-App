ede37bb5a7a6738668259724eed25c4caf3d78ed
leodefarias <lf25079@gmail.com>
2024-09-04 22:04:55 -0300

    archives correction

---
+++ src/pages/Home.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Card from '../components/Card'; // Importando o componente Card
+import '../styles/Home.css'; // CSS específico para a Home
+
+const Home = () => {
+  return (
+    <div className="container">
+
+      <div className="home-banner position-relative">
+        <div>
+          <h1>Crie seus vídeos online</h1>
+          <p>Transforme suas ideias em filmes memoráveis: onde a criatividade encontra a simplicidade.</p>
+          <a href="#start" className="primary-button">Começar agora!</a>
+        </div>
+        <div className="image-container">
+          <img src="/src/assets/images/home-image.png" alt="Hero" className="hero-image" />
+        </div>
+      </div>
+
+      <div className="cards-container">
+        <Card
+          bgColor="#F1C2B0"
+          title="Youtube"
+          icon="/src/assets/icons/youtube.png"
+          number="01"
+          description="Produza conteúdo cativante e conquiste seu público com vídeos de alta qualidade no maior palco digital do mundo."
+        />
+
+        <Card
+          bgColor="#F9EB98"
+          title="Tiktok"
+          icon="/src/assets/icons/tiktok.png"
+          number="02"
+          description="Faça sua criatividade brilhar em vídeos curtos e envolventes que vão cativar a comunidade global do TikTok."
+        />
+
+        <Card
+          bgColor="#C0D9DD"
+          title="Facebook"
+          icon="/src/assets/icons/facebook.png"
+          number="03"
+          description="Conecte-se com sua audiência de forma autêntica e impactante através de vídeos que se destacam no feed do Facebook."
+        />
+
+        <Card
+          bgColor="#6975E8"
+          title="Instagram"
+          icon="/src/assets/icons/instagram-card.png"
+          number="04"
+          description="Compartilhe suas histórias de maneira única e conquiste milhões de likes no Instagram utilizando Stories e Reels."
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/Sobre.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import '../styles/Sobre.css';
+
+const Sobre = () => {
+  return (
+    <div className="sobre-container">
+      <div className="text-and-images">
+        <div className="text-section">
+          <h1>Bem-vindo à revolução dos vídeos!</h1>
+          <p>
+            Somos uma startup com o objetivo de facilitar a produção de conteúdo em larga escala com qualidade profissional, capaz de atingir uma audiência de milhões de seguidores.
+          </p>
+          <button className="primary-button">Baixe o app</button>
+        </div>
+
+        <div className="image-section">
+          <div className="image" style={{ backgroundImage: "url('/src/assets/images/sobre-image1.png')" }}></div>
+          <div className="image" style={{ backgroundImage: "url('/src/assets/images/sobre-image2.png')" }}></div>
+        </div>
+      </div>
+
+      <div className="planos-section">
+        <h2>Planos</h2>
+        <div className="planos-content">
+          {/* Plano Individual */}
+          <div className="plano individual">
+            <div className="header">Individual</div>
+            <div className="content">
+              <p className="usuario">1 Usuário</p>
+              <div className="dropdown">
+                <select>
+                  <option>10 Vídeos R$15</option>
+                  <option>20 Vídeos R$25</option>
+                  <option>30 Vídeos R$35</option>
+                </select>
+              </div>
+            </div>
+            <div className="button-container">
+              <button className="button">Cadastrar</button>
+            </div>
+          </div>
+
+          {/* Plano Profissional - Times */}
+          <div className="plano profissional">
+            <div className="header">Profissional - Times</div>
+            <div className="content">
+              <p className="usuario">1-10 Usuários</p>
+              <div className="dropdown">
+                <select>
+                  <option>Vídeos Ilimitados R$40</option>
+                  <option>Vídeos Ilimitados R$60</option>
+                  <option>Vídeos Ilimitados R$80</option>
+                </select>
+              </div>
+              <p className="usuario">+10 Usuários</p>
+              <div className="dropdown">
+                <select>
+                  <option>Vídeos Ilimitados R$20</option>
+                  <option>Vídeos Ilimitados R$30</option>
+                  <option>Vídeos Ilimitados R$40</option>
+                </select>
+              </div>
+            </div>
+            <div className="button-container">
+              <button className="button">Cadastrar</button>
+            </div>
+          </div>
+
+          {/* Plano Corporativo */}
+          <div className="plano corporativo">
+            <div className="header">Corporativo</div>
+            <div className="content">
+              <img src="/src/assets/images/desenho-pessoa-celular.png" alt="Desenho de uma pessoa ao lado de um celular" className="corporativo-image" />
+            </div>
+            <div className="button-container">
+              <button className="button">Entrar Em Contato</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sobre;
